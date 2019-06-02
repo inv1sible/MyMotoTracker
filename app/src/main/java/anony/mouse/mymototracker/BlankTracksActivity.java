7b0d6742fcdae182305b73913cb6ec2ed789a12f
Andreas Dhein <4n6@dhein.com>
2019-06-02 22:47:31 +0200

    connecting activity to blank fragment and slide out to the left

--- app/src/main/java/anony/mouse/mymototracker/BlankTracksActivity.java
+++ app/src/main/java/anony/mouse/mymototracker/BlankTracksActivity.java
@@ -1,6 +1,7 @@
 package anony.mouse.mymototracker;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -20,5 +21,18 @@ public class BlankTracksActivity extends AppCompatActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_tracks);
+
+        // Get the Intent that started this activity and extract the string
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+
+    }
+
+    @Override
+    public void finish() {
+        super.finish();
+        overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
     }
 }
